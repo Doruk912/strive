983d3e36b5db7078b3bcfb111f40319f93956a3c
Doruk Ege Aksu <82142325+Doruk912@users.noreply.github.com>
2025-02-28 11:54:36 +0300

    Added padding on top to account for header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { Box } from '@mui/material';
+import './App.css';
 
 const theme = createTheme({
     palette: {
