d22062d435c8f8f1674a3cfa78a75c1bddbfb2b1
Doruk Ege Aksu <82142325+Doruk912@users.noreply.github.com>
2025-03-26 15:52:59 +0300

    Increase password hash cost factor

--- schema.sql
+++ schema.sql
@@ -14,14 +14,10 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Original passwords are included as comments:
--- admin@strive.com: password = '123'
--- manager@strive.com: password = '123'
--- john@example.com: password = '123'
--- jane@example.com: password = '123'
+-- Passwords: '123456'
 
 INSERT INTO users (email, password, first_name, last_name, phone, role) VALUES
-('admin@strive.com', '$2a$10$Ew/1gdMDiqB1.KRj1HOJNeCWxR9JANm9a8f1z0nCj32bnGHHfLZaO', 'Admin', 'User', '+1234567890', 'ADMIN'),
-('manager@strive.com', '$2a$10$pB.4F.oIBGBV4NcLJGYbqu4J1MdAoDYJ6dtNgzIr8wUXnLyKYoN9C', 'Manager', 'User', '+1234567893', 'MANAGER'),
-('john@example.com', '$2a$10$2aNM1KPRPdPJd8vyfg/oReGqFqGJOKwJrsrYNVkY6LeQ7QC4jnmtm', 'John', 'Doe', '+1234567891', 'CUSTOMER'),
-('jane@example.com', '$2a$10$Iv4OfXAUW8EbFR7cMQA.XuMW0ZMjZWIUEYwE/q/GQqmrGsW6RV6dW', 'Jane', 'Smith', '+1234567892', 'CUSTOMER')
+('admin@strive.com', '$2a$12$KMJjGzb6tX93GDD3djKJueVQpOkOP.iPzEMWRAZGlIimKel5Pw9nm', 'Admin', 'User', '+1234567890', 'ADMIN'),
+('manager@strive.com', '$2a$12$KMJjGzb6tX93GDD3djKJueVQpOkOP.iPzEMWRAZGlIimKel5Pw9nm', 'Manager', 'User', '+1234567893', 'MANAGER'),
+('john@example.com', '$2a$12$KMJjGzb6tX93GDD3djKJueVQpOkOP.iPzEMWRAZGlIimKel5Pw9nm', 'John', 'Doe', '+1234567891', 'CUSTOMER'),
+('jane@example.com', '$2a$12$KMJjGzb6tX93GDD3djKJueVQpOkOP.iPzEMWRAZGlIimKel5Pw9nm', 'Jane', 'Smith', '+1234567892', 'CUSTOMER')
